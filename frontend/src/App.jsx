daf310b5e58496f61fc05c54cda2a9e47af2991e
FOA-developer <flourishagbanoma@gmail.com>
2026-05-10 05:16:56 +0100

    fix: got rid of the blob cursor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,18 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import Hero from './Hero.jsx';
 import Services from "./Services.jsx";
-import Projects from  "./Projects.jsx";
+import Projects from "./Projects.jsx";
 import Footer from "./Footer.jsx";
-import BlobCursor from "./Blob.jsx";
 
-const App= () => {
-  
+
+const App = () => {
+
   return (
     <>
-      <BlobCursor
-        blobType="circle"
-        fillColor="#FF4D00"
-        trailCount={1}
-        sizes={[43]}
-        innerSizes={[20]}
-        innerColor="#FF4D00"
-        opacities={[0.45]}
-        shadowColor="rgba(0,0,0,0.75)"
-        shadowBlur={5}
-        shadowOffsetX={10}
-        shadowOffsetY={10}
-        filterStdDeviation={30}
-        useFilter={true}
-        fastDuration={0.1}
-        slowDuration={0.5}
-        zIndex={100}
-      />
-      <Hero/>
-      <Services/>
-      <Projects/>
+      <Hero />
+      <Services />
+      <Projects />
     </>
   );
 }
- export default App;
+export default App;
